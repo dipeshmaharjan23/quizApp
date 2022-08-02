d3dccb1486d04f207d288384907a27c5b2ba90e9
dipesh maharjan <max.maharjan.2014@gmail.com>
2022-08-02 07:41:44 +0545

    completed

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
- const questionEL = document.getElementById('question')
+const questionEL = document.getElementById('question')
 const a_choice = document.getElementById('a_choice');
 const b_choice = document.getElementById('b_choice');
 const c_choice = document.getElementById('c_choice');
 const d_choice = document.getElementById('d_choice');
-const btn= document.querySelector(" .submit-btn");
- 
-console.log(d_choice)
+const btn = document.querySelector(" .submit-btn");
+const quiz = document.getElementById('quiz')
 
- let nextQuestion = 0 ;
+const ans = document.querySelectorAll('.ans')
 
-const questData= [
+
+let nextQuestion = 0;
+let score = 0;
+
+const questData = [
     {
         question: "Which is the popular language?",
         a: "Java",
@@ -19,34 +22,74 @@ const questData= [
         correct: "d"
     },
     {
-        question : "Which language is the oldest language ?",
+        question: "Which language is the oldest language ?",
         a: "Java",
         b: "C",
         c: "Phython",
         d: "JavaScript",
-        correct: "c"
+        correct: "b"
     },
     {
-        question: "Who is the richest person in the world ?" ,
-        a : "Elon Musk",
+        question: "Who is the richest person in the world ?",
+        a: "Elon Musk",
         b: "Bill Gates ",
-        c : "Anil Ambani",
-        d : "None of them",
-        correct : "a"
+        c: "Anil Ambani",
+        d: "None of them",
+        correct: "a"
     }
 ]
 
-function quest(){
- const currentQuiz = questData[nextQuestion];
- questionEL.innerText =currentQuiz.question ;
- a_choice.innerText = currentQuiz.a;
- b_choice.innerText = currentQuiz.b;
- c_choice.innerText = currentQuiz.c;
- d_choice.innerText = currentQuiz.d;
+function quest() {
+    deselect();
+    const currentQuiz = questData[nextQuestion];
+    questionEL.innerText = currentQuiz.question;
+    a_choice.innerText = currentQuiz.a;
+    b_choice.innerText = currentQuiz.b;
+    c_choice.innerText = currentQuiz.c;
+    d_choice.innerText = currentQuiz.d;
 
- nextQuestion++;
+
 }
-
 quest();
 
-btn.addEventListener('click',quest)
+function select() {
+let answer = undefined;
+
+    ans.forEach((item) => {
+        if (item.checked) {
+            answer = item.id;
+        }
+    })
+    return answer;
+}
+
+function deselect() {
+
+    ans.forEach((item) => {
+        item.checked = false;
+    })
+}
+
+
+
+btn.addEventListener('click', () => {
+    const answer = select();
+    if (answer) {
+        if (answer === questData[nextQuestion].correct) {
+            score++
+        }
+        nextQuestion++;
+
+        if (nextQuestion < questData.length) {
+
+            quest();
+        }
+        else {
+            quiz.innerHTML = `<h3> You scored ${score} out of ${questData.length}</h3> 
+
+             <button onclick="location.reload()" class="btn">Reload</button>`
+        }
+    }
+
+
+})
